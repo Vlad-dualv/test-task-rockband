6404edc3621c25509d4c5263279609071a621671
Vlad <vladswork95@gmail.com>
2025-09-20 13:26:34 +0100

    func: contact form submission

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ const mobileMenu = document.querySelector('.mobile-menu-wrapper');
 const burgerButton = document.querySelector('.burger-menu');
 const closeButton = document.querySelector('.mobile-menu-close-btn');
 const menuLinks = document.querySelectorAll('.mobile-menu-link');
+const contactForm = document.getElementById('contactForm');
 
-// Function to toggle modal and scroll
 function toggleModal() {
   mobileMenu.classList.toggle('is-open');
   document.body.classList.toggle('modal-open');
@@ -14,14 +14,46 @@ function toggleModal() {
 burgerButton.addEventListener('click', toggleModal);
 closeButton.addEventListener('click', toggleModal);
 
-// Close modal when clicking on menu links
 menuLinks.forEach(link => {
   link.addEventListener('click', toggleModal);
 });
 
-// Close modal when clicking outside
 mobileMenu.addEventListener('click', e => {
   if (e.target === mobileMenu) {
     toggleModal();
+  }
+});
+
+// Form submission handling
+
+contactForm.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const formData = new FormData(contactForm);
+  const data = {
+    name: formData.get('name').trim(),
+    email: formData.get('email').trim(),
+    message: formData.get('message').trim(),
+  };
+
+  const queryString = new URLSearchParams(data).toString();
+  const apiUrl = `https://httpbin.org/get?${queryString}`;
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        ContentType: 'application/json',
+      },
+    });
+    if (response.ok) {
+      console.log(data);
+      alert('Дякуємо! Ваше повідомлення успішно надіслано.');
+      contactForm.reset();
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    alert('Вибачте, сталася помилка при відправці форми. Спробуйте пізніше.');
   }
 });
